a7026a060b22a7c1d0cba0c697468bad343bdf84
Tgordonmiller <83499380+Tgordonmiller@users.noreply.github.com>
2022-07-23 17:34:42 -0400

    Create codeer.js

---
+++ codeer.js
@@ -0,0 +1,26 @@
+class StrictEqualityExtension {
+  getInfo() {
+    return {
+      id: 'codeer', // change this if you make an actual extension!
+      name: 'codeer',
+      blocks: [
+        {
+          opcode: 'strictlyEquals',
+          blockType: Scratch.BlockType.COMMAND,
+          text: 'set to [ONE]',
+          arguments: {
+            ONE: {
+              type: Scratch.ArgumentType.MATRIX
+              defaultValue: 'matrix'
+            }
+          }
+        }
+      ]
+    };
+  }
+  strictlyEquals(args) {
+    // Note strict equality: Inputs must match exactly: in type, case, etc.
+    m = args.ONE
+  }
+}
+Scratch.extensions.register(new codeer());
